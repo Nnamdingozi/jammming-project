0e1d3a679a50e283febfc8fd1736629c55f7f4af
joerald <ngozika1105@gmail.com>
2024-01-12 20:17:53 +0100

    create folders for components adn style searchbar

---
+++ src/SearchBar/SearchBar..js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './SearchBar.module.css'
+
+export default function SearchBar() {
+    return (
+        <div className={styles.banner}>
+            <input type="text" className={styles.input}></input>
+            <button className={styles.search} >Search</button>
+        </div>
+
+    )
+}
