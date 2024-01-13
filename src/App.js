c10b087ce11677ca436654020db3775eea72265b
joerald <ngozika1105@gmail.com>
2024-01-13 21:28:19 +0100

    add static style for Playlist and modified SearchResults

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar/SearchBar.";
 import SearchResults from "./searchResults/SearchResults";
-import './App.css'
+import './App.css';
+import PlayList from "./playList/Playlist";
 
 function App() {
   return (
-   <>
+   <div className="banner">
    <h1><span style={{color:'beige'}} >JA</span><span style={{color:'rgb(235, 142, 208)', fontSize: '2.4rem'}}>MMM</span><span style={{color:'beige'}}>ING</span></h1>
    <SearchBar />
+   <div className="container">
    <SearchResults />
-   </>
+   <PlayList />
+   </div>
+   
+   </div>
   );
 }
 
--- src/searchResults/SearchResults.js
+++ src/searchResults/SearchResults.js
@@ -13,6 +13,7 @@ export default function SearchResults(){
     })
     return (
         <div className={styles.searchResults}>
+    <h3>Search Results</h3>
             <ul>{songs}</ul>
 
         </div>
